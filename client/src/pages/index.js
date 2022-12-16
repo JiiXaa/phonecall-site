2cbb1f4d4669086225e3dd9876777fbab772d1bf
ElyseRH <elyserhoran@gmail.com>
2022-12-16 19:22:50 +0000

    Remove videosdk for second test

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,6 +2,5 @@ import Homepage from './Homepage';
 import Register from './Register';
 import Error from './Error';
 import Dashboard from './Dashboard';
-import Call from './Call';
 
-export { Homepage, Register, Error, Dashboard, Call };
+export { Homepage, Register, Error, Dashboard };
