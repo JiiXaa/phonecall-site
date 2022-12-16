ae69e04687cbfc444c29934f44fa23052d50439d
lmflolo <linnmarieflolo@gmail.com>
2022-12-16 19:04:04 +0000

    stripped down everything to just input fields for full name, email, password and submit button. added h2, h3 and p elements, and hr

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,37 +2,17 @@ const Register = () => {
   return (
     <>
       <div className='classes here'>
-        <h1>Register</h1>
+        <h1>Sign Up</h1>
+        <h2>Create a free account with your email.</h2>
         <form>
-          <fieldset>
-            <legend>Personal Details</legend>
-            <label for="fname">First Name:</label>
-            <input type="text" id="fname" name="first_name" required/>
-            <label for="lname">Last Name:</label>
-            <input type="text" id="lname" name="last_name"/>
-            <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email" required/>
-          </fieldset>
-          <fieldset>
-            <legend>Account Details</legend>
-            <label for="username">Create a username:</label>
-            <input type="text" id="username" name="username" required/>
-            <label for="password1">Create a password:</label>
-            <input type="password" id="password1" name="password1" required/>
-            <label for="password2">Confirm your password:</label>
-            <input type="password" id="password2" name="password2" required/>
-          </fieldset>
-          <fieldset>
-            <legend>Your Location</legend>
-            <label for="time_zone">Your time zone:</label>
-
-            <label for="country">Your country of residence:</label>
-
-            <label for="city">Your city of residence:</label>
-            <input type="text" id="city" name="city"/>
-          </fieldset>
-          <button type="submit"></button>
+          <input type="text" id="full_name" name="full_name" placeholder="Full Name" required/>
+          <input type="email" id="email" name="email" placeholder="Email" required/>
+          <input type="password" id="password" name="password" placeholder="Password" required/>
+          <button type="submit">Create your free account</button>
         </form>
+        <p>By continuing, you agree to our Terms of Service.<br/>Read our Privacy Policy.</p>
+        <hr/>
+        <h3>Already have an account? Sign in</h3>
       </div>
     </>
   );
