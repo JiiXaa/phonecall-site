287cf7d04d8f682a386f44234261e879a0ee4d1f
lmflolo <linnmarieflolo@gmail.com>
2022-12-16 12:22:08 +0000

    added fieldset and input fields for account details

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,6 +15,17 @@ const Register = () => {
             <label for="email">E-mail:</label>
             <input type="email" id="email" name="email" required>
           </fieldset>
+          <fieldset>
+            <legend>Account details</legend>
+            <label for="username">Create a username:</label>
+            <input type="text" id="username" name="username" required>
+            <br>
+            <label for="password1">Create a password:</label>
+            <input type="password" id="password1" name="password1" required>
+            <br>
+            <label for="password2">Confirm your password:</label>
+            <input type="password" id="password2" name="password2" required>
+          </fieldset>
         </form>
       </div>
     </>
