ea609ede4645483d7940894053d82b45cb699b40
JiiXaa <anythingfx@gmail.com>
2022-12-16 16:50:16 +0000

    Added loginUser and registerUser for the server routes. Also server is connected to the MongoDB database. All tests passed in the Insomnia app.

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
+import 'express-async-errors';
 
 const port = process.env.PORT || 5000;
+
+// db and authenticate User
+import connectDB from './db/connect.js';
 
 // routes
 import authRouter from './routes/authRoutes.js';
@@ -14,6 +18,8 @@ app.use('/api/auth', authRouter);
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
+    // spins server up only if we are connected to the mongo db
     app.listen(port, () => {
       console.log(`Server is running on port ${port}...`);
     });
@@ -22,5 +28,5 @@ const start = async () => {
   }
 };
 
-// Start the server
+// connect to the db and starts express
 start();
