683be6887d86769ed27f50952ca894c3be27fbbd
lmflolo <linnmarieflolo@gmail.com>
2022-12-16 11:31:27 +0000

    added required to first name and email fields

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -7,13 +7,13 @@ const Register = () => {
           <fieldset>
             <legend>Personal details</legend>
             <label for="fname">First Name:</label>
-            <input type="text" id="fname" name="first_name">
+            <input type="text" id="fname" name="first_name" required>
             <br>
             <label for="lname">Last Name:</label>
             <input type="text" id="lname" name="last_name">
             <br>
             <label for="email">E-mail:</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" required>
           </fieldset>
         </form>
       </div>
