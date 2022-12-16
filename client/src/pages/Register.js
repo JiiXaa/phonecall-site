6fa0d63d15fe5023561070d1656980c974ee1c82
lmflolo <linnmarieflolo@gmail.com>
2022-12-16 11:23:09 +0000

    started registration form: added fields for first and last name

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,7 +3,16 @@ const Register = () => {
     <>
       <div className='classes here'>
         <h1>Register</h1>
-        <h2>Test PR</h2>
+        <form>
+          <fieldset>
+            <legend>Personal details</legend>
+            <label for="fname">First Name:</label>
+            <input type="text" id="fname" name="first_name">
+            <br>
+            <label for="lname">Last Name:</label>
+            <input type="text" id="lname" name="last_name">
+          </fieldset>
+        </form>
       </div>
     </>
   );
