7114c8d4bf739a6f65136c010eb44610f4bae667
JiiXaa <anythingfx@gmail.com>
2022-12-17 14:23:06 +0000

    App refactored to vanilla style.

--- client/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Homepage from './Homepage';
-import Register from './Register';
-import Error from './Error';
-import Dashboard from './Dashboard';
-
-export { Homepage, Register, Error, Dashboard };
--- db/connect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = (url) => {
-  return mongoose.set('strictQuery', false), mongoose.connect(url);
-};
-
-export default connectDB;
