36daa2d953f5e3015d33cad35e3c1c6670eb2b7e
lmflolo <linnmarieflolo@gmail.com>
2022-12-17 08:43:59 +0000

    add homepage image file, import on homepage and add img element

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,3 +1,6 @@
+
+import homepage_image from './main-homepage-image.jpg';
+
 const Homepage = () => {
   return (
     <>
@@ -6,6 +9,7 @@ const Homepage = () => {
         <h2>Sign Up. Connect. Chat.</h2>
         <button>Sign Up</button>
         <button>Log In</button>
+        <img src={homepage_image} alt="three people sitting in front of a christmas tree and smiling"/>
       </div>
     </>
   );
