89cfe8a15935e75df58ef9382c0458c81488000b
lmflolo <linnmarieflolo@gmail.com>
2022-12-17 11:32:19 +0000

    import bootstrap and add bootstrap classes to homepage elements

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,16 +1,29 @@
 
-import homepage_image from './main-homepage-image.jpg';
+import homepage_image from '../assets/images/main-homepage-image.jpg';
 
 const Homepage = () => {
   return (
     <>
-      <div className='main_homepage_div'>
-        <h1>Forge new friendships this festive season!</h1>
-        <h2>Sign Up. Connect. Chat.</h2>
-        <button>Sign Up</button>
-        <button>Log In</button>
-        <img src={homepage_image} alt="three people sitting in front of a christmas tree and smiling"/>
-      </div>
+      <section id="title" className='title-section'>
+
+        <div className='container-fluid title-section-container'>
+
+          <div className="row">
+            
+            <div className="col-lg-6">
+              <h1 className="big-heading">Forge new friendships this festive season!</h1>
+              <h2>Sign Up. Connect. Chat.</h2>
+              <button type="button" className="btn btn-lg btn-dark signup-btn">Sign Up</button>
+              <button type="button" className="btn btn-lg btn-outline-light signup-btn">Log In</button>
+            </div>
+
+            <div className="col-lg-6">
+              <img className="homepage-image" src={homepage_image} alt="three people sitting in front of a christmas tree and smiling"/>
+            </div>
+
+          </div>
+        </div>
+      </section>
     </>
   );
 };
