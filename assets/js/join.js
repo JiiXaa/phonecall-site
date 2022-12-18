61224ed28547075bdfc2a504d8f6aae3febb17be
ElyseRH <elyserhoran@gmail.com>
2022-12-18 14:17:07 +0000

    Update node modules

--- assets/js/join.js
+++ assets/js/join.js
@@ -12,18 +12,3 @@ createBtn.addEventListener("click", function () {
 
 
 
-
-
-
-// // getMeetingIds()
-
-// function getMeetingIds() {
-//     let see_id = document.getElementById("meetingid");
-//     console.log('meetingId', see_id);
-//     // console.log(get_id)
-//     // document.getElementById("join-call").textContent = get_id
-//     // console.log(get_id)
-// }
-// // // document.addEventListener
-// // // document.getElementById("join-call").textContent = "hey"
-// getMeetingIds()
