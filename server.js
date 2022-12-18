c9c09e8858be73cfc5b786edc17e896c490eeefd
JiiXaa <anythingfx@gmail.com>
2022-12-18 15:47:52 +0000

    express server tests

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express()
+const bodyParser = require('body-parser');
 var fs = require('fs');
 
 // middleware  
 
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // STEP 1: Read the existing data from json file  
 
@@ -12,9 +13,9 @@ let meeting_data = require("./meeting-data.json")
 
 // API routes 
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + './join-call.html');
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/join-call.html');
+// })
 
 // test this after
 // app.get('/', function (req, res) {
@@ -32,20 +33,20 @@ app.get('/', (req, res) => {
 //   });
 // });
 
-app.post("/", function (req, res) {
+app.post("/api/meetings", function (req, res) {
 
   let join_id = req.body.join_id;
-
+  console.log(join_id)
   let joinMeetingData = {
-    join_id,
+    join_id
   }
 
   res.send(joinMeetingData)
 
-  // STEP 2: add new user data to users object using push method  
+  // STEP 2: add new user data to users object using push method
   meeting_data.push(joinMeetingData)
 
-  // STEP 3: Writing data in a JSON file 
+/ // STEP 3: Writing data in a JSON file 
 
   fs.writeFile('meeting-data.json', JSON.stringify(meeting_data), err => {
     if (err) throw err
@@ -53,7 +54,7 @@ app.post("/", function (req, res) {
     console.log("Done writting JSON file")
   })
 
-  // STEP 4: Write the new info in the text file named message
+ // STEP 4: Write the new info in the text file named message
 
   fs.writeFile('./meeting-data.txt', JSON.stringify(meeting_data), err => {
     if (err) throw err
